70b6d33c49f2d0df16451acd08eb902a5a46b9f3
Francis Chan <hchan154@my.bcit.ca>
2019-01-02 18:33:27 -0800

    Added side bar to map

--- js/access_map.js
+++ js/access_map.js
@@ -1,23 +1,35 @@
 let myUrl = window.location.href;
 let delimiter = "?";
-let searchIndex = myUrl.indexOf(delimiter)+1;
+let searchIndex = myUrl.indexOf(delimiter) + 1;
 let searchTerm = myUrl.substring(searchIndex);
 
-function imgReplace(){
-    if (searchTerm == 'test'){
-       let myMap = document.getElementById('access-map');
-       myMap.src = "../img/wheelchair_library.png";
+function imgReplace() {
+    if (searchTerm == 'test') {
+        let myMap = document.getElementById('access-map');
+        myMap.src = "../img/wheelchair_library.png";
     }
 }
 
-function directionText(){
-    if (searchTerm == 'test'){
+function directionText() {
+    if (searchTerm == 'test') {
         let directions = document.createElement('ul');
         let step_1 = document.createElement('li');
         let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
         step_1.createTextNode(text);
         directions.appendChild(step_1);
         document.body.append(directions);
+    }
+}
+
+function toggleNav(){
+    let navWidth = document.getElementById('mySidebar').offsetWidth;
+    if (navWidth == "0"){
+        document.getElementById("mySidebar").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+    }
+    else {
+        document.getElementById("mySidebar").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
     }
 }
 imgReplace();
