892fa9bfc17a160910a2d9c02acb70bdba000c87
Darryl Chen <pchen121@my.bcit.ca>
2019-01-03 12:01:31 -0800

    New

--- js/mobile.js
+++ js/mobile.js
@@ -6,7 +6,7 @@ function toMap(mapType) {
   console.log(mapType);
 
   let map = document.getElementById("currMap");
-  map.src = "./img/"+mapType;
+  map.src = "./img/" + mapType;
   // map.style.overflowX = "scroll";
 }
 
@@ -14,7 +14,7 @@ function parseInputs() {
   let location = document.getElementById("locationInput");
   let target = document.getElementById("targetInput");
 
-  if (location.value == 'Bus Stop' && target.value == 'Library'){
+  if (location.value == 'Bus Stop' && target.value == 'Library') {
     toMap("wheelchair_library.png");
   } else if (location.value == 'SE02' && target.value == 'Tim Hortons') {
     toMap("wheelchair_tim_hortons.png");
@@ -34,22 +34,22 @@ function openNav() {
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("main").style.marginLeft = "0";
 }
 
-$("#yourLocation").click(function() {
+$("#yourLocation").click(function () {
   document.getElementById("locationInput").value = "Bus Stop";
 });
 
 function directionText() {
   let directions = document.getElementById("directions");
   directions.innerHTML =
-      "<br>&nbsp;&nbsp;Cross White Avenue.<br><br>\
-                          &nbsp;&nbsp;Turn right and walk for<br> &nbsp;&nbsp;500 meters.<br><br>\
-                          &nbsp;&nbsp;Turn left and walk for<br>&nbsp;&nbsp; 250 meters using the ramp. <br><br>\
-                          &nbsp;&nbsp;Go through SE12 and take<br>&nbsp;&nbsp; a right.<br><br>\
-                          &nbsp;&nbsp;Take the ramp to reach<br>&nbsp;&nbsp; 2nd floor.<br><br>\
-                          &nbsp;&nbsp;Your destination is <br>&nbsp;&nbsp;on the right.\
+    "<br>Cross White Avenue.<br><br>\
+                          Turn right and walk for<br> 500 meters.<br><br>\
+                          Turn left and walk for<br> 250 meters using the ramp. <br><br>\
+                          Go through SE12 and take<br> a right.<br><br>\
+                          Take the ramp to reach<br> 2nd floor.<br><br>\
+                          Your destination is <br>on the right.\
                           ";
-  }
-  directionText();
+}
+directionText();
